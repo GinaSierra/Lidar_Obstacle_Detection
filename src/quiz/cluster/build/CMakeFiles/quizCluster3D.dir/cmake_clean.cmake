3c0224db55d3177d4f263e12b823a349d1a9f0ec
GinaSierra <ginaks@gmail.com>
2021-06-20 13:45:35 -0700

    First commit Message

---
+++ src/quiz/cluster/build/CMakeFiles/quizCluster3D.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quizCluster3D.dir/cluster3D.cpp.o"
+  "CMakeFiles/quizCluster3D.dir/home/workspace/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o"
+  "quizCluster3D.pdb"
+  "quizCluster3D"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quizCluster3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/quiz/ransac/build/CMakeFiles/quizRansacPlane.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quizRansacPlane.dir/ransac3d.cpp.o"
+  "CMakeFiles/quizRansacPlane.dir/home/workspace/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o"
+  "quizRansacPlane.pdb"
+  "quizRansacPlane"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quizRansacPlane.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
